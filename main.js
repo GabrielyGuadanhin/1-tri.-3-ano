9205baec58edf59178c989fcad9297063026e71e
Gabriely <106533204+GabrielyGuadanhin@users.noreply.github.com>
2024-04-16 08:17:55 -0300

    D

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ for(let i = 0; i < botoes.length; i++) {
      textos[i].classList.add("ativo");
         }    
     }
-    const contadores = document.querySelectorAll
-    const tempoObjetivo =new Date (2025-01-01)
+    const contadores = document.querySelectorAll(".contador");
+    const tempoObjetivo =new Date ("2025-01-28T00:00:00");
     let tempoAtual = new Date();
 
-    contadores[0].textContent = tempoObjetivo
+    contadores[0].textContent = tempoObjetivo - tempoAtual;
